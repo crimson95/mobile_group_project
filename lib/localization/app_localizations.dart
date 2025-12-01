02a1eda13afc1efa3628fa3af73288ca486d5634
Manilyn8 <vice0021@algonquinlive.com>
2025-12-01 14:06:11 -0500

    rename

---
+++ lib/localization/app_localizations.dart
@@ -0,0 +1,53 @@
+// TODO Implement this library.
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+  _AppLocalizationsDelegate();
+
+  late Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    // assets/translations/en.json OR fil.json
+    final String jsonString = await rootBundle
+        .loadString('assets/translations/${locale.languageCode}.json');
+    final Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings =
+        jsonMap.map((key, value) => MapEntry(key, value.toString()));
+    return true;
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key] ?? key;
+  }
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) =>
+      ['en', 'fil'].contains(locale.languageCode); // English + Filipino
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    final localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
